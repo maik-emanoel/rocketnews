75f96e1e0f98d4db678fe023a5b6d406bced2c57
Maik Emanoel <105499887+maik-emanoel@users.noreply.github.com>
2023-03-21 20:18:49 -0300

    add validation with regex on email input

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 const form = document.querySelector('form')
+const emailInput = document.querySelector('#email')
+const btn = document.querySelector('.btn')
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
-  alert('Email enviado com sucesso!')
 })
+
+btn.addEventListener('click', () => {
+  const emailValue = emailInput.value
+
+  if(!validEmail(emailValue)) {
+    alert('Por favor, digite um email válido!')
+  } else {
+    alert('Email enviado com sucesso!')
+  }
+})
+
+function validEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return regex.test(email)
+}
+
+
